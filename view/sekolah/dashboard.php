6cc23314989961b0781c096166ee7d9ed13164f6
sitijuniyanti <sitijuniyanti@gmail.com>
2020-07-12 21:54:26 +0700

    halaman sekolah

--- view/sekolah/dashboard.php
+++ view/sekolah/dashboard.php
@@ -2,12 +2,11 @@
   <!-- Content Header (Page header) -->
   <section class="content-header">
     <h1>
-      Dashboard
-      <small>Control panel</small>
+      Home
+      <small>Website Sekolah</small>
     </h1>
     <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-      <li class="active">Dashboard</li>
+      <li class="active"><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
     </ol>
   </section>
 
@@ -18,61 +17,15 @@
       require_once "../../helper/flash_message.php";
       ?>
     </div>
-    <!-- Small boxes (Stat box) -->
-    <div class="row">
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-aqua">
-          <div class="inner">
-            <h3>20</h3>
+    <p> Ini adalah halaman website Roboteach untuk sekolah agar sekolah dapat memonitoring keberadaan pengajar</p>
 
-            <p>Data Pengajar</p>
-          </div>
-          <!-- <div class="icon">
-              <i class="ion ion-bag"></i>
-            </div> -->
-          <a href="#" class="small-box-footer">Detail <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-          <div class="inner">
-            <h3>20</h3>
+    <!-- ./col -->
+</div>
+<!-- /.row -->
+<!-- Main row -->
 
-            <p>Data Sekolah</p>
-          </div>
-          <!-- <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div> -->
-          <a href="#" class="small-box-footer">Detail <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-yellow">
-          <div class="inner">
-            <h3>100</h3>
+<!-- /.row (main row) -->
 
-            <p>Data Peralatan</p>
-          </div>
-          <!-- <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div> -->
-          <a href="#" class="small-box-footer">Detail <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-      </div>
-      <!-- ./col -->
-
-      <!-- ./col -->
-    </div>
-    <!-- /.row -->
-    <!-- Main row -->
-
-    <!-- /.row (main row) -->
-
-  </section>
-  <!-- /.content -->
+</section>
+<!-- /.content -->
 </div>
--- view/sekolah/index.php
+++ view/sekolah/index.php
@@ -55,16 +55,16 @@ if (isset($_SESSION['level']) && $_SESSION['level'] == "SEKOLAH") {
             <!-- User Account: style can be found in dropdown.less -->
             <li class="dropdown user user-menu">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <img src="<?= baseUrl('resource/img/admin.jpg'); ?>" class="user-image" alt="User Image">
-                <span class="hidden-xs">Admin</span>
+                <img src="<?= baseUrl('resource/img/usersekolah.png'); ?>" class="user-image" alt="User Image">
+                <span class="hidden-xs">SD 01</span>
               </a>
               <ul class="dropdown-menu">
                 <!-- User image -->
                 <li class="user-header">
-                  <img src="<?= baseUrl('resource/img/admin.jpg'); ?>" class="img-circle" alt="User Image">
+                  <img src="<?= baseUrl('resource/img/usersekolah.png'); ?>" class="img-circle" alt="User Image">
 
                   <p>
-                    Admin
+                    SD 01
                     <small>Member since Nov. 2012</small>
                   </p>
                 </li>
